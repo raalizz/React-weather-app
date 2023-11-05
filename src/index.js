73888bcc3ad00013165bf5660bd08de8ebfd8b2f
raalizz <135476716+raalizz@users.noreply.github.com>
2023-11-05 14:45:53 +0100

    I added a body.css file and added some styling to it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./body.css";
 import App from "./App";
 import Weather from "./Weather";
 import reportWebVitals from "./reportWebVitals";
