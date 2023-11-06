c7754e98ae8f65bf821d04f80cbaf8b5f17f33de
raalizz <135476716+raalizz@users.noreply.github.com>
2023-11-06 02:04:19 +0100

    I created the tempInfo and the forecastInfo and added css styling to them and also worked on the responsiveness of the website

--- src/Weather.js
+++ src/Weather.js
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 import axios from "axios";
 import ReactAnimatedWeather from "react-animated-weather";
 
-export default function Weather() {
+import "./Weather.css";
+
+export default function Weather(props) {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
+  const [loaded, setLoaded] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
     const apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
     const units = "metric";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(displayWeather);
   }
 
   function displayWeather(response) {
+    setLoaded(true);
     setWeather({
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
@@ -24,7 +28,7 @@ export default function Weather() {
         <ReactAnimatedWeather
           icon="RAIN"
           color="Black"
-          size={30}
+          size={60}
           animate={true}
         />
       ),
@@ -34,28 +38,121 @@ export default function Weather() {
   function updateCity(event) {
     setCity(event.target.value);
   }
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="row">
-          <div className="col-sm">
-            <form onSumbit={handleSubmit}>
-              <input
-                type="search"
-                placeholder="Search a city"
-                onChange={updateCity}
-              />
-              <input type="submit" value="search" />
-            </form>
-          </div>
-          <div className="col-sm">
-            <button>
-              {" "}
-              <i class="fa-solid fa-location-crosshairs"></i>{" "}
-            </button>
-          </div>
+
+  const form = (
+    <div className="row">
+      <div className="col-sm">
+        <form onSumbit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Search a city"
+            onChange={updateCity}
+            autocomplete="off"
+            className="searchBar"
+          />
+        </form>
+      </div>
+      <div className="col-sm">
+        <button className="search-btn">
+          <h7 className="searchFont">Search</h7>
+        </button>
+      </div>
+      <div className="col-sm">
+        <button className="location-btn">
+          {" "}
+          <i className="fa-solid fa-location-crosshairs locationIcon"></i>{" "}
+        </button>
+      </div>
+    </div>
+  );
+
+  const tempInfo = (
+    <div className="row">
+      <div className="col-sm-6 mt-4">
+        <h1>{city} </h1>
+        <h6> {weather.description}</h6>
+      </div>
+      <div className="col-sm-3 mt-4">{weather.icon}</div>
+      <div className="col-sm-3 mt-4">
+        <span className="units">
+          <a href="/" className="active">
+            ˚C{" "}
+          </a>{" "}
+          |<a href="/">˚F</a>
+        </span>
+        <br />
+        <h7> {weather.temperature} </h7>
+      </div>
+    </div>
+  );
+
+  const forecastInfo = (
+    <div className="forecast-container">
+      <div className="forecast">
+        <div>
+          <i className="fa-solid fa-temperature-three-quarters forecastIcons"></i>
+          <br />
+          <strong>{weather.temperature}</strong>˚C
+          <br />
+          <h7>Feels like</h7>
+        </div>
+        <div>
+          <i className="fa-solid fa-droplet forecastIcons"></i>
+          <br />
+          <strong>{weather.humidity}</strong>%
+          <br />
+          <h7>Humidity</h7>
+        </div>
+        <div>
+          <i className="fa-solid fa-wind forecastIcons"></i>
+          <br />
+          <strong>{weather.wind}</strong>m/s
+          <br />
+          <h7>Wind </h7>
         </div>
       </div>
     </div>
   );
+
+  if (loaded) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="wrapper">
+            {form}
+            {tempInfo}
+            {forecastInfo}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="wrapper">
+            {form}
+            <div className="row">
+              <div className="col-sm-6 mt-4">
+                <h1>{props.city} </h1>
+                <h6> {weather.description}</h6>
+              </div>
+              <div className="col-sm-3 mt-4">{weather.icon}</div>
+              <div className="col-sm-3 mt-4">
+                <span className="units">
+                  <a href="/" className="active">
+                    ˚C{" "}
+                  </a>{" "}
+                  |<a href="/">˚F</a>
+                </span>
+                <br />
+                <h7> {weather.temperature} </h7>
+              </div>
+            </div>
+            {forecastInfo}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
