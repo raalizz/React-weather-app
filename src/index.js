5e65cbd724b6a0d02ccc91f2d100b08a0260ecf9
raalizz <135476716+raalizz@users.noreply.github.com>
2023-11-07 13:00:43 +0100

    I deleted the body.css and added its content to the weather.css instead

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./body.css";
+
 import Footer from "./Footer";
 import Weather from "./Weather";
 import reportWebVitals from "./reportWebVitals";
