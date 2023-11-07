a535cc02330be3991c1d475e2d59d7fdced366f4
raalizz <135476716+raalizz@users.noreply.github.com>
2023-11-07 12:43:02 +0100

    deleted bootstrap on the index.html and added it through the terminal instead and imported on the weather.js page

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 import ReactAnimatedWeather from "react-animated-weather";
 
 import "./Weather.css";
