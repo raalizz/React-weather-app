d884c33378a7cf89a27eee03e779938e2aab1c55
raalizz <135476716+raalizz@users.noreply.github.com>
2023-11-19 14:47:32 +0100

    I added the return null to the if statement

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -24,6 +24,8 @@ export default function DailyForecast(props) {
                     <WeatherForecastDay data={dailyForecast} />
                   </div>
                 );
+              } else {
+                return null;
               }
             })}
           </div>
